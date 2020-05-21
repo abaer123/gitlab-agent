831200f3ab7e7c163fd1e83ae57eab0e8860abf9
Mikhail Mazurskiy <mmazurskiy@gitlab.com>
2020-05-21 00:52:32 +0000

    Configuration fetching

--- cmd/agentg/agentgapp/app.go
+++ cmd/agentg/agentgapp/app.go
@@ -11,6 +11,9 @@ import (
 	"gitlab.com/gitlab-org/cluster-integration/kubernetes-management-ng/gitlab-agent/cmd"
 	"gitlab.com/gitlab-org/cluster-integration/kubernetes-management-ng/gitlab-agent/pkg/agentg"
 	"gitlab.com/gitlab-org/cluster-integration/kubernetes-management-ng/gitlab-agent/pkg/agentrpc"
+	gitalyauth "gitlab.com/gitlab-org/gitaly/auth"
+	"gitlab.com/gitlab-org/gitaly/client"
+	"gitlab.com/gitlab-org/gitaly/proto/go/gitalypb"
 	"google.golang.org/grpc"
 )
 
@@ -21,20 +24,39 @@ const (
 type App struct {
 	ListenNetwork             string
 	ListenAddress             string
+	GitalyAddress             string
+	GitalyToken               string
 	ReloadConfigurationPeriod time.Duration
 }
 
 func (a *App) Run(ctx context.Context) error {
+	// Gitaly client
+	var gitalyOpts []grpc.DialOption
+	if a.GitalyToken != "" {
+		gitalyOpts = append(gitalyOpts, grpc.WithPerRPCCredentials(gitalyauth.RPCCredentialsV2(a.GitalyToken)))
+	}
+	gitalyConn, err := client.DialContext(ctx, a.GitalyAddress, gitalyOpts)
+	if err != nil {
+		return fmt.Errorf("gRPC.dial Gitaly: %v", err)
+	}
+	defer gitalyConn.Close()
+
+	// Main logic of Agentg
+	srv := &agentg.Agent{
+		ReloadConfigurationPeriod: a.ReloadConfigurationPeriod,
+		CommitServiceClient:       gitalypb.NewCommitServiceClient(gitalyConn),
+	}
+
+	// gRPC server
 	lis, err := net.Listen(a.ListenNetwork, a.ListenAddress)
 	if err != nil {
 		return fmt.Errorf("listen: %v", err)
 	}
 	var opts []grpc.ServerOption
 	grpcServer := grpc.NewServer(opts...)
-	srv := &agentg.Agent{
-		ReloadConfigurationPeriod: a.ReloadConfigurationPeriod,
-	}
 	agentrpc.RegisterGitLabServiceServer(grpcServer, srv)
+
+	// Start things up
 	st := stager.New()
 	defer st.Shutdown()
 	stage := st.NextStageWithContext(ctx)
@@ -49,6 +71,8 @@ func NewFromFlags(flagset *flag.FlagSet, arguments []string) (cmd.Runnable, erro
 	app := &App{}
 	flagset.StringVar(&app.ListenNetwork, "listen-network", "", "Network type to listen on")
 	flagset.StringVar(&app.ListenAddress, "listen-address", "", "Address to listen on")
+	flagset.StringVar(&app.GitalyAddress, "gitaly-address", "", "Gitaly address")
+	flagset.StringVar(&app.GitalyToken, "gitaly-token", "", "Gitaly authentication token")
 	flagset.DurationVar(&app.ReloadConfigurationPeriod, "reload-configuration-period", defaultReloadConfigurationPeriod, "How often to reload agentk configuration")
 	if err := flagset.Parse(arguments); err != nil {
 		return nil, err
--- pkg/agentg/agentg.go
+++ pkg/agentg/agentg.go
@@ -1,42 +1,115 @@
 package agentg
 
 import (
+	"context"
+	"fmt"
+	"io"
+	"os"
 	"time"
 
 	"gitlab.com/gitlab-org/cluster-integration/kubernetes-management-ng/gitlab-agent/pkg/agentrpc"
+	"gitlab.com/gitlab-org/cluster-integration/kubernetes-management-ng/gitlab-agent/pkg/api"
+	"gitlab.com/gitlab-org/gitaly/proto/go/gitalypb"
+	"google.golang.org/protobuf/encoding/protojson"
+	"k8s.io/apimachinery/pkg/util/wait"
+	"sigs.k8s.io/yaml"
 )
 
 type Agent struct {
 	ReloadConfigurationPeriod time.Duration
+	CommitServiceClient       gitalypb.CommitServiceClient
 }
 
 func (a *Agent) GetConfiguration(req *agentrpc.ConfigurationRequest, configStream agentrpc.GitLabService_GetConfigurationServer) error {
 	ctx := configStream.Context()
-	t := time.NewTicker(a.ReloadConfigurationPeriod)
-	defer t.Stop()
-	for {
-		select {
-		case <-ctx.Done():
-			return nil
-		case <-t.C:
-			err := a.sendConfiguration(configStream)
-			if err != nil {
-				return err
-			}
-		}
-	}
-}
-
-func (a *Agent) sendConfiguration(configStream agentrpc.GitLabService_GetConfigurationServer) error {
-	config, err := a.fetchConfiguration()
+	agentInfo, err := api.AgentInfoFromContext(ctx)
 	if err != nil {
-		// TODO log
-		return nil // don't want to close the response stream, so report no error
+		return err
 	}
-	return configStream.Send(config)
+	err = wait.PollImmediateUntil(a.ReloadConfigurationPeriod, a.sendConfiguration(agentInfo, configStream), ctx.Done())
+	if err == wait.ErrWaitTimeout {
+		return nil // all good, ctx is done
+	}
+	return err
 }
 
-func (a *Agent) fetchConfiguration() (*agentrpc.ConfigurationResponse, error) {
-	// TODO fetch configuration from per-agent git repo
-	return &agentrpc.ConfigurationResponse{}, nil
+func (a *Agent) sendConfiguration(agentInfo *api.AgentInfo, configStream agentrpc.GitLabService_GetConfigurationServer) wait.ConditionFunc {
+	return func() (bool /*done*/, error) {
+		config, err := a.fetchConfiguration(configStream.Context(), agentInfo)
+		if err != nil {
+			// TODO log
+			fmt.Fprintf(os.Stderr, "Error fetching configuration: %v\n", err)
+			return false, nil // don't want to close the response stream, so report no error
+		}
+		return false, configStream.Send(config)
+	}
+}
+
+// fetchConfiguration fetches agent's configuration from a corresponding repository.
+// Assumes configuration is stored in "agents/<agent id>/config.yaml" file.
+func (a *Agent) fetchConfiguration(ctx context.Context, agentInfo *api.AgentInfo) (*agentrpc.ConfigurationResponse, error) {
+	// TODO make an API call to GitLab RoR. Send agentInfo.Id, agentInfo.ProjectId, agentInfo.Token
+
+	repo := &gitalypb.Repository{
+		StorageName:  "default",                                                                            // TODO fetch from GitLab RoR via API call
+		RelativePath: "@hashed/19/58/19581e27de7ced00ff1ce50b2047e7a567c76b1cbaebabe5ef03f7c3017bb5b7.git", // TODO fetch from GitLab RoR via API call
+		//GitObjectDirectory:            "",
+		//GitAlternateObjectDirectories: nil,
+		GlRepository:  "",               // TODO fetch from GitLab RoR via API call
+		GlProjectPath: "root/test-proj", // TODO fetch from GitLab RoR via API call
+	}
+
+	//findCommitReq := &gitalypb.FindCommitRequest{
+	//	Repository: repo,
+	//	Revision:   []byte("master"), // TODO handle different default branch
+	//}
+	//fcResp, err := a.CommitServiceClient.FindCommit(ctx, findCommitReq)
+	//if err != nil {
+	//	return nil, err
+	//}
+
+	// mimicking lib/gitlab/gitaly_client/commit_service.rb#tree_entry
+	treeEntryReq := &gitalypb.TreeEntryRequest{
+		Repository: repo,
+		Revision:   []byte("master"), //fcResp.Commit.Id
+		Path:       []byte(fmt.Sprintf("agents/%s/config.yaml", agentInfo.Id)),
+		Limit:      1024 * 1024,
+	}
+	teResp, err := a.CommitServiceClient.TreeEntry(ctx, treeEntryReq)
+	if err != nil {
+		return nil, fmt.Errorf("TreeEntry: %v", err)
+	}
+	var configYaml []byte
+	for {
+		entry, err := teResp.Recv()
+		if err != nil {
+			if err == io.EOF {
+				break
+			}
+			return nil, fmt.Errorf("TreeEntry.Recv: %v", err)
+		}
+		configYaml = append(configYaml, entry.Data...)
+	}
+	configJson, err := yaml.YAMLToJSON(configYaml)
+	if err != nil {
+		return nil, fmt.Errorf("TreeEntry.YAMLToJSON: %v", err)
+	}
+	configFile := &agentrpc.ConfigurationFile{}
+	err = protojson.Unmarshal(configJson, configFile)
+	if err != nil {
+		return nil, fmt.Errorf("TreeEntry.protojson.Unmarshal: %v", err)
+	}
+	agentConfig, err := extractAgentConfiguration(configFile)
+	if err != nil {
+		return nil, fmt.Errorf("extract agent configuration: %v", err)
+	}
+	return &agentrpc.ConfigurationResponse{
+		Configuration: agentConfig,
+	}, nil
+}
+
+func extractAgentConfiguration(file *agentrpc.ConfigurationFile) (*agentrpc.AgentConfiguration, error) {
+	return &agentrpc.AgentConfiguration{
+		SomeFeatureEnabled: file.SomeFeatureEnabled,
+	}, nil
 }
