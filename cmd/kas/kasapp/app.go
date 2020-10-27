f52943e325627694af35bb886fe03999c4be6b30
Mikhail Mazurskiy <mmazurskiy@gitlab.com>
2020-10-27 16:17:23 +1100

    Use enums

--- cmd/kas/kasapp/app.go
+++ cmd/kas/kasapp/app.go
@@ -34,8 +34,12 @@ func (a *App) Run(ctx context.Context) error {
 		return err
 	}
 	ApplyDefaultsToKasConfigurationFile(cfg)
-	if a.ListenNetwork != defaultListenNetwork {
-		cfg.Listen.Network = a.ListenNetwork
+	if a.ListenNetwork != defaultListenNetwork.String() {
+		val, ok := kascfg.ListenNetworkEnum_value[a.ListenNetwork]
+		if !ok {
+			return fmt.Errorf("unsupported listen-network flag passed: %s", a.ListenNetwork)
+		}
+		cfg.Listen.Network = kascfg.ListenNetworkEnum(val)
 	}
 	if a.ListenAddress != defaultListenAddress {
 		cfg.Listen.Address = a.ListenAddress
@@ -99,7 +103,7 @@ func LoadConfigurationFile(configFile string) (*kascfg.ConfigurationFile, error)
 func NewFromFlags(flagset *pflag.FlagSet, arguments []string) (cmd.Runnable, error) {
 	app := &App{}
 	flagset.StringVar(&app.ConfigurationFile, "configuration-file", "", "Optional configuration file to use (YAML)")
-	flagset.StringVar(&app.ListenNetwork, "listen-network", defaultListenNetwork, "Network type to listen on. Supported values: tcp, tcp4, tcp6, unix")
+	flagset.StringVar(&app.ListenNetwork, "listen-network", defaultListenNetwork.String(), "Network type to listen on. Supported values: tcp, tcp4, tcp6, unix")
 	flagset.StringVar(&app.ListenAddress, "listen-address", defaultListenAddress, "Address to listen on")
 	flagset.BoolVar(&app.ListenWebSocket, "listen-websocket", false, "Enable \"gRPC through WebSocket\" listening mode. Rather than expecting gRPC directly, expect a WebSocket connection, from which a gRPC stream is then unpacked")
 	flagset.StringVar(&app.GitLabAddress, "gitlab-address", defaultGitLabAddress, "GitLab address")
@@ -112,7 +116,7 @@ func NewFromFlags(flagset *pflag.FlagSet, arguments []string) (cmd.Runnable, err
 }
 
 func loggerFromConfig(loggingCfg *kascfg.LoggingCF) (*zap.Logger, error) {
-	level, err := logz.LevelFromString(loggingCfg.Level)
+	level, err := logz.LevelFromString(loggingCfg.Level.String())
 	if err != nil {
 		return nil, err
 	}
