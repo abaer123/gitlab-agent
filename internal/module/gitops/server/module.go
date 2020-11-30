c40c51637cf760095cf57bc0985825e305b086ac
Mikhail Mazurskiy <mmazurskiy@gitlab.com>
2020-12-01 09:27:50 +1100

    Move objectsToSynchronizeVisitor in a separate file

--- internal/module/gitops/server/module.go
+++ internal/module/gitops/server/module.go
@@ -2,12 +2,10 @@ package server
 
 import (
 	"context"
-	"path"
 	"regexp"
 	"strings"
 	"time"
 
-	"github.com/bmatcuk/doublestar/v2"
 	"gitlab.com/gitlab-org/cluster-integration/gitlab-agent/internal/api"
 	"gitlab.com/gitlab-org/cluster-integration/gitlab-agent/internal/api/apiutil"
 	"gitlab.com/gitlab-org/cluster-integration/gitlab-agent/internal/gitaly"
@@ -204,76 +202,6 @@ func (m *module) getProjectInfo(ctx context.Context, log *zap.Logger, agentMeta 
 		err = nil // don't want to close the response stream, so report no error
 	}
 	return nil, err, true
-}
-
-type objectsToSynchronizeVisitor struct {
-	server                 rpc.Gitops_GetObjectsToSynchronizeServer
-	glob                   string
-	remainingTotalFileSize int64
-	fileSizeLimit          int64
-	maxNumberOfFiles       uint32
-	numberOfFiles          uint32
-	sendFailed             bool
-}
-
-func (v *objectsToSynchronizeVisitor) Entry(entry *gitalypb.TreeEntry) (bool /* download? */, int64 /* max size */, error) {
-	if v.numberOfFiles == v.maxNumberOfFiles {
-		return false, 0, errz.NewUserErrorf("maximum number of manifest files limit reached: %d", v.maxNumberOfFiles)
-	}
-	v.numberOfFiles++
-	filename := string(entry.Path)
-	if isHiddenDir(filename) {
-		return false, 0, nil
-	}
-	shouldDownload, err := doublestar.Match(v.glob, filename)
-	if err != nil {
-		return false, 0, errz.NewUserErrorWithCausef(err, "glob %s match failed", v.glob)
-	}
-	return shouldDownload, minInt64(v.remainingTotalFileSize, v.fileSizeLimit), nil
-}
-
-func (v *objectsToSynchronizeVisitor) StreamChunk(path []byte, data []byte) (bool /* done? */, error) {
-	v.remainingTotalFileSize -= int64(len(data))
-	if v.remainingTotalFileSize < 0 {
-		// This should never happen because we told Gitaly the maximum file size that we'd like to get.
-		// i.e. we should have gotten an error from Gitaly if file is bigger than the limit.
-		return false, status.Error(codes.Internal, "unexpected negative remaining total file size")
-	}
-	err := v.server.Send(&rpc.ObjectsToSynchronizeResponse{
-		Message: &rpc.ObjectsToSynchronizeResponse_Object_{
-			Object: &rpc.ObjectsToSynchronizeResponse_Object{
-				Source: string(path),
-				Data:   data,
-			},
-		},
-	})
-	if err != nil {
-		v.sendFailed = true
-	}
-	return false, err
-}
-
-// isHiddenDir checks if a file is in a directory, which name starts with a dot.
-func isHiddenDir(filename string) bool {
-	dir := path.Dir(filename)
-	if dir == "." { // root directory special case
-		return false
-	}
-	parts := strings.Split(dir, "/")
-	for _, part := range parts {
-		if strings.HasPrefix(part, ".") {
-			return true
-		}
-	}
-	return false
-}
-
-func minInt64(a, b int64) int64 {
-	if a < b {
-		return a
-	}
-
-	return b
 }
 
 func globToGitaly(glob string) ([]byte /* repoPath */, bool /* recursive */, string /* glob */) {
