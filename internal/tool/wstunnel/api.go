ede96fe1a5dc07984ea7b8940f09863a902c501d
Mikhail Mazurskiy <mmazurskiy@gitlab.com>
2020-12-27 17:42:49 +1100

    Remove unused function

--- internal/tool/wstunnel/api.go
+++ internal/tool/wstunnel/api.go
@@ -1,16 +1,7 @@
 package wstunnel
 
-import "net/http"
-
 const (
 	// TunnelWebSocketProtocol is a subprotocol that allows client and server recognize each other.
 	// See https://tools.ietf.org/html/rfc6455#section-11.3.4.
 	TunnelWebSocketProtocol = "ws-tunnel"
-	webSocketProtocolHeader = "Sec-WebSocket-Protocol"
 )
-
-func ProtocolHeader() http.Header {
-	header := http.Header{}
-	header.Set(webSocketProtocolHeader, TunnelWebSocketProtocol)
-	return header
-}
