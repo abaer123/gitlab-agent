07ea9ac9f2565710ad740628345fd68830e7cd01
Mikhail Mazurskiy <mmazurskiy@gitlab.com>
2021-02-04 14:15:54 +1100

    Unit tests for Agent Tracker
    Redis integration tests are done on ExpiringHash, there is no need for them here too

--- internal/module/agent_tracker/tracker_test.go
+++ internal/module/agent_tracker/tracker_test.go
@@ -2,28 +2,21 @@ package agent_tracker
 
 import (
 	"context"
-	"math/rand"
-	"os"
-	"strconv"
+	"errors"
 	"testing"
 	"time"
 
-	"github.com/go-redis/redis/v8"
+	"github.com/golang/mock/gomock"
 	"github.com/google/go-cmp/cmp"
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
 	"gitlab.com/gitlab-org/cluster-integration/gitlab-agent/internal/module/modshared"
 	"gitlab.com/gitlab-org/cluster-integration/gitlab-agent/internal/tool/redistool"
+	"gitlab.com/gitlab-org/cluster-integration/gitlab-agent/internal/tool/testing/mock_redis"
 	"go.uber.org/zap/zaptest"
-	"google.golang.org/protobuf/proto"
 	"google.golang.org/protobuf/testing/protocmp"
 	"google.golang.org/protobuf/types/known/anypb"
 	"google.golang.org/protobuf/types/known/timestamppb"
-)
-
-const (
-	redisURLEnvName = "REDIS_URL"
-	ttl             = time.Second
 )
 
 var (
@@ -32,149 +25,298 @@ var (
 	_ Tracker    = &RedisTracker{}
 )
 
-func TestRegisterConnection(t *testing.T) {
-	client, tr, keyPrefix := setupTracker(t)
+func TestRegisterConnection_HappyPath(t *testing.T) {
+	ctx, cancel := context.WithCancel(context.Background())
+	defer cancel()
+	r, byAgentId, byProjectId, info := setupTracker(t)
 
-	// Given
-	info := connInfo()
+	byProjectId.EXPECT().
+		Set(gomock.Any(), info.ProjectId, info.ConnectionId, gomock.Any())
+	byAgentId.EXPECT().
+		Set(gomock.Any(), info.AgentId, info.ConnectionId, gomock.Any()).
+		Do(func(ctx context.Context, key interface{}, hashKey int64, value *anypb.Any) {
+			cancel()
+		})
 
-	// When
-	require.NoError(t, tr.registerConnection(context.Background(), info))
+	go func() {
+		assert.True(t, r.RegisterConnection(context.Background(), info))
+	}()
 
-	// Then
-	equalHash(t, client, connectionsByProjectIdHashKey(keyPrefix)(info.ProjectId), info)
-	equalHash(t, client, connectionsByAgentIdHashKey(keyPrefix)(info.AgentId), info)
+	require.NoError(t, r.Run(ctx))
 }
 
-func TestUnregisterConnection(t *testing.T) {
-	client, tr, keyPrefix := setupTracker(t)
+func TestRegisterConnection_BothCalledOnError(t *testing.T) {
+	ctx, cancel := context.WithCancel(context.Background())
+	defer cancel()
+	r, byAgentId, byProjectId, info := setupTracker(t)
 
-	// Given
-	info := connInfo()
+	byProjectId.EXPECT().
+		Set(gomock.Any(), info.ProjectId, info.ConnectionId, gomock.Any()).
+		Return(errors.New("err1"))
+	byAgentId.EXPECT().
+		Set(gomock.Any(), info.AgentId, info.ConnectionId, gomock.Any()).
+		DoAndReturn(func(ctx context.Context, key interface{}, hashKey int64, value *anypb.Any) error {
+			cancel()
+			return errors.New("err2")
+		})
 
-	// When
-	require.NoError(t, tr.registerConnection(context.Background(), info))
-	require.NoError(t, tr.unregisterConnection(context.Background(), info))
+	go func() {
+		assert.True(t, r.RegisterConnection(context.Background(), info))
+	}()
 
-	// Then
-	require.Empty(t, getHash(t, client, connectionsByProjectIdHashKey(keyPrefix)(info.ProjectId)))
-	require.Empty(t, getHash(t, client, connectionsByAgentIdHashKey(keyPrefix)(info.AgentId)))
+	require.NoError(t, r.Run(ctx))
 }
 
-func TestHashExpires(t *testing.T) {
-	t.Parallel()
-	client, tr, keyPrefix := setupTracker(t)
+func TestUnregisterConnection_HappyPath(t *testing.T) {
+	ctx, cancel := context.WithCancel(context.Background())
+	defer cancel()
+	r, byAgentId, byProjectId, info := setupTracker(t)
 
-	// Given
-	info := connInfo()
+	gomock.InOrder(
+		byProjectId.EXPECT().
+			Set(gomock.Any(), info.ProjectId, info.ConnectionId, gomock.Any()),
+		byProjectId.EXPECT().
+			Unset(gomock.Any(), info.ProjectId, info.ConnectionId),
+	)
+	gomock.InOrder(
+		byAgentId.EXPECT().
+			Set(gomock.Any(), info.AgentId, info.ConnectionId, gomock.Any()),
+		byAgentId.EXPECT().
+			Unset(gomock.Any(), info.AgentId, info.ConnectionId).
+			Do(func(ctx context.Context, key interface{}, hashKey int64) {
+				cancel()
+			}),
+	)
 
-	// When
-	require.NoError(t, tr.registerConnection(context.Background(), info))
-	time.Sleep(ttl + 100*time.Millisecond)
+	go func() {
+		assert.True(t, r.RegisterConnection(context.Background(), info))
+		assert.True(t, r.UnregisterConnection(context.Background(), info))
+	}()
 
-	// Then
-	require.Empty(t, getHash(t, client, connectionsByProjectIdHashKey(keyPrefix)(info.ProjectId)))
-	require.Empty(t, getHash(t, client, connectionsByAgentIdHashKey(keyPrefix)(info.AgentId)))
+	require.NoError(t, r.Run(ctx))
 }
 
-func TestGC(t *testing.T) {
-	t.Parallel()
-	client, tr, keyPrefix := setupTracker(t)
+func TestUnregisterConnection_BothCalledOnError(t *testing.T) {
+	ctx, cancel := context.WithCancel(context.Background())
+	defer cancel()
+	r, byAgentId, byProjectId, info := setupTracker(t)
 
-	// Given
-	info := connInfo()
+	gomock.InOrder(
+		byProjectId.EXPECT().
+			Set(gomock.Any(), info.ProjectId, info.ConnectionId, gomock.Any()),
+		byProjectId.EXPECT().
+			Unset(gomock.Any(), info.ProjectId, info.ConnectionId).
+			Return(errors.New("err1")),
+	)
+	gomock.InOrder(
+		byAgentId.EXPECT().
+			Set(gomock.Any(), info.AgentId, info.ConnectionId, gomock.Any()),
+		byAgentId.EXPECT().
+			Unset(gomock.Any(), info.AgentId, info.ConnectionId).
+			DoAndReturn(func(ctx context.Context, key interface{}, hashKey int64) error {
+				cancel()
+				return errors.New("err1")
+			}),
+	)
 
-	// When
-	require.NoError(t, tr.registerConnection(context.Background(), info))
-	_, err := client.Pipelined(context.Background(), func(p redis.Pipeliner) error {
-		newExpireIn := 3 * ttl
-		p.PExpire(context.Background(), connectionsByProjectIdHashKey(keyPrefix)(info.ProjectId), newExpireIn)
-		p.PExpire(context.Background(), connectionsByAgentIdHashKey(keyPrefix)(info.AgentId), newExpireIn)
-		return nil
+	go func() {
+		assert.True(t, r.RegisterConnection(context.Background(), info))
+		assert.True(t, r.UnregisterConnection(context.Background(), info))
+	}()
+
+	require.NoError(t, r.Run(ctx))
+}
+
+func TestGC_HappyPath(t *testing.T) {
+	r, byAgentId, byProjectId, _ := setupTracker(t)
+
+	byAgentId.EXPECT().
+		GC(gomock.Any()).
+		Return(2, nil)
+
+	byProjectId.EXPECT().
+		GC(gomock.Any()).
+		Return(1, nil)
+
+	removed, err := r.runGc(context.Background())
+	assert.NoError(t, err)
+	assert.EqualValues(t, 3, removed)
+}
+
+func TestGC_BothCalledOnError(t *testing.T) {
+	r, byAgentId, byProjectId, _ := setupTracker(t)
+
+	byAgentId.EXPECT().
+		GC(gomock.Any()).
+		Return(2, errors.New("err1"))
+
+	byProjectId.EXPECT().
+		GC(gomock.Any()).
+		Return(1, errors.New("err2"))
+
+	removed, err := r.runGc(context.Background())
+	assert.Error(t, err)
+	assert.EqualValues(t, 3, removed)
+}
+
+func TestRefresh_HappyPath(t *testing.T) {
+	r, byAgentId, byProjectId, _ := setupTracker(t)
+
+	byAgentId.EXPECT().
+		Refresh(gomock.Any())
+	byProjectId.EXPECT().
+		Refresh(gomock.Any())
+	assert.NoError(t, r.refreshRegistrations(context.Background()))
+}
+
+func TestRefresh_BothCalledOnError(t *testing.T) {
+	r, byAgentId, byProjectId, _ := setupTracker(t)
+
+	byAgentId.EXPECT().
+		Refresh(gomock.Any()).
+		Return(errors.New("err1"))
+	byProjectId.EXPECT().
+		Refresh(gomock.Any()).
+		Return(errors.New("err2"))
+	assert.Error(t, r.refreshRegistrations(context.Background()))
+}
+
+func TestGetConnectionsByProjectId_HappyPath(t *testing.T) {
+	r, _, byProjectId, info := setupTracker(t)
+	any, err := anypb.New(info)
+	require.NoError(t, err)
+	byProjectId.EXPECT().
+		Scan(gomock.Any(), info.ProjectId, gomock.Any()).
+		Do(func(ctx context.Context, key interface{}, cb redistool.ScanCallback) (int, error) {
+			var done bool
+			done, err = cb(any, nil)
+			if err != nil || done {
+				return 0, err
+			}
+			return 0, nil
+		})
+	var cbCalled int
+	err = r.GetConnectionsByProjectId(context.Background(), info.ProjectId, func(i *ConnectedAgentInfo) (done bool, err error) {
+		cbCalled++
+		assert.Empty(t, cmp.Diff(i, info, protocmp.Transform()))
+		return false, nil
 	})
 	require.NoError(t, err)
-	time.Sleep(ttl + 100*time.Millisecond)
-	deletedKeys, err := tr.runGc(context.Background())
-	require.NoError(t, err)
-	assert.EqualValues(t, 2, deletedKeys)
-
-	// Then
-	require.Empty(t, getHash(t, client, connectionsByProjectIdHashKey(keyPrefix)(info.ProjectId)))
-	require.Empty(t, getHash(t, client, connectionsByAgentIdHashKey(keyPrefix)(info.AgentId)))
+	assert.EqualValues(t, 1, cbCalled)
 }
 
-func TestRefresh(t *testing.T) {
-	t.Parallel()
-	client, tr, keyPrefix := setupTracker(t)
-
-	// Given
-	info := connInfo()
-
-	// When
-	require.NoError(t, tr.registerConnection(context.Background(), info))
-	registrationTime := time.Now()
-	time.Sleep(ttl / 2)
-	err := tr.refreshRegistrations(context.Background())
-	require.NoError(t, err)
-
-	// Then
-	expireAfter := registrationTime.Add(ttl)
-	valuesExpireAfter(t, client, connectionsByProjectIdHashKey(keyPrefix)(info.ProjectId), expireAfter)
-	valuesExpireAfter(t, client, connectionsByAgentIdHashKey(keyPrefix)(info.AgentId), expireAfter)
-}
-
-func TestGetConnectionsEmpty(t *testing.T) {
-	_, tr, _ := setupTracker(t)
-
-	// Given
-	info := connInfo()
-
-	// When
-	// no registered connections
-
-	// Then
-	var infos ConnectedAgentInfoCollector
-	err := tr.GetConnectionsByProjectId(context.Background(), info.ProjectId, infos.Collect)
-	require.NoError(t, err)
-	assert.Empty(t, infos)
-	infos = nil
-	err = tr.GetConnectionsByAgentId(context.Background(), info.AgentId, infos.Collect)
-	require.NoError(t, err)
-	assert.Empty(t, infos)
-}
-
-func TestGetConnections(t *testing.T) {
-	_, tr, _ := setupTracker(t)
-
-	// Given
-	info := connInfo()
-
-	// When
-	require.NoError(t, tr.registerConnection(context.Background(), info))
-
-	// Then
-	var infos ConnectedAgentInfoCollector
-	err := tr.GetConnectionsByProjectId(context.Background(), info.ProjectId, infos.Collect)
-	require.NoError(t, err)
-	assert.Empty(t, cmp.Diff([]*ConnectedAgentInfo{info}, []*ConnectedAgentInfo(infos), protocmp.Transform()))
-	infos = nil
-	err = tr.GetConnectionsByAgentId(context.Background(), info.AgentId, infos.Collect)
-	require.NoError(t, err)
-	assert.Empty(t, cmp.Diff([]*ConnectedAgentInfo{info}, []*ConnectedAgentInfo(infos), protocmp.Transform()))
-}
-
-func setupTracker(t *testing.T) (redis.UniversalClient, *RedisTracker, string) {
-	client := redisClient(t)
-	t.Cleanup(func() {
-		assert.NoError(t, client.Close())
+func TestGetConnectionsByProjectId_ScanError(t *testing.T) {
+	r, _, byProjectId, info := setupTracker(t)
+	byProjectId.EXPECT().
+		Scan(gomock.Any(), info.ProjectId, gomock.Any()).
+		Do(func(ctx context.Context, key interface{}, cb redistool.ScanCallback) (int, error) {
+			done, err := cb(nil, errors.New("intended error"))
+			require.NoError(t, err)
+			assert.False(t, done)
+			return 0, nil
+		})
+	err := r.GetConnectionsByProjectId(context.Background(), info.ProjectId, func(i *ConnectedAgentInfo) (done bool, err error) {
+		require.FailNow(t, "unexpected call")
+		return false, nil
 	})
-	r := rand.New(rand.NewSource(time.Now().UnixNano()))
-	prefix := make([]byte, 32)
-	_, err := r.Read(prefix)
 	require.NoError(t, err)
-	keyPrefix := string(prefix)
-	tr := NewRedisTracker(zaptest.NewLogger(t), client, keyPrefix, ttl, time.Minute, time.Minute)
-	return client, tr, keyPrefix
+}
+
+func TestGetConnectionsByProjectId_UnmarshalError(t *testing.T) {
+	r, _, byProjectId, info := setupTracker(t)
+	byProjectId.EXPECT().
+		Scan(gomock.Any(), info.ProjectId, gomock.Any()).
+		Do(func(ctx context.Context, key interface{}, cb redistool.ScanCallback) (int, error) {
+			done, err := cb(&anypb.Any{
+				TypeUrl: "gitlab.agent.agent_tracker.ConnectedAgentInfo", // valid
+				Value:   []byte{1, 2, 3},                                 // invalid
+			}, nil)
+			require.NoError(t, err) // ignores error to keep going
+			assert.False(t, done)
+			return 0, nil
+		})
+	err := r.GetConnectionsByProjectId(context.Background(), info.ProjectId, func(i *ConnectedAgentInfo) (done bool, err error) {
+		require.FailNow(t, "unexpected call")
+		return false, nil
+	})
+	require.NoError(t, err)
+}
+
+func TestGetConnectionsByAgentId_HappyPath(t *testing.T) {
+	r, byAgentId, _, info := setupTracker(t)
+	any, err := anypb.New(info)
+	require.NoError(t, err)
+	byAgentId.EXPECT().
+		Scan(gomock.Any(), info.AgentId, gomock.Any()).
+		Do(func(ctx context.Context, key interface{}, cb redistool.ScanCallback) (int, error) {
+			var done bool
+			done, err = cb(any, nil)
+			if err != nil || done {
+				return 0, err
+			}
+			return 0, nil
+		})
+	var cbCalled int
+	err = r.GetConnectionsByAgentId(context.Background(), info.AgentId, func(i *ConnectedAgentInfo) (done bool, err error) {
+		cbCalled++
+		assert.Empty(t, cmp.Diff(i, info, protocmp.Transform()))
+		return false, nil
+	})
+	require.NoError(t, err)
+	assert.EqualValues(t, 1, cbCalled)
+}
+
+func TestGetConnectionsByAgentId_ScanError(t *testing.T) {
+	r, byAgentId, _, info := setupTracker(t)
+	byAgentId.EXPECT().
+		Scan(gomock.Any(), info.AgentId, gomock.Any()).
+		Do(func(ctx context.Context, key interface{}, cb redistool.ScanCallback) (int, error) {
+			done, err := cb(nil, errors.New("intended error"))
+			require.NoError(t, err)
+			assert.False(t, done)
+			return 0, nil
+		})
+	err := r.GetConnectionsByAgentId(context.Background(), info.AgentId, func(i *ConnectedAgentInfo) (done bool, err error) {
+		require.FailNow(t, "unexpected call")
+		return false, nil
+	})
+	require.NoError(t, err)
+}
+
+func TestGetConnectionsByAgentId_UnmarshalError(t *testing.T) {
+	r, byAgentId, _, info := setupTracker(t)
+	byAgentId.EXPECT().
+		Scan(gomock.Any(), info.AgentId, gomock.Any()).
+		Do(func(ctx context.Context, key interface{}, cb redistool.ScanCallback) (int, error) {
+			done, err := cb(&anypb.Any{
+				TypeUrl: "gitlab.agent.agent_tracker.ConnectedAgentInfo", // valid
+				Value:   []byte{1, 2, 3},                                 // invalid
+			}, nil)
+			require.NoError(t, err) // ignores error to keep going
+			assert.False(t, done)
+			return 0, nil
+		})
+	err := r.GetConnectionsByAgentId(context.Background(), info.AgentId, func(i *ConnectedAgentInfo) (done bool, err error) {
+		require.FailNow(t, "unexpected call")
+		return false, nil
+	})
+	require.NoError(t, err)
+}
+
+func setupTracker(t *testing.T) (*RedisTracker, *mock_redis.MockExpiringHashInterface, *mock_redis.MockExpiringHashInterface, *ConnectedAgentInfo) {
+	ctrl := gomock.NewController(t)
+	byAgentId := mock_redis.NewMockExpiringHashInterface(ctrl)
+	byProjectId := mock_redis.NewMockExpiringHashInterface(ctrl)
+	tr := &RedisTracker{
+		log:                    zaptest.NewLogger(t),
+		refreshPeriod:          time.Minute,
+		gcPeriod:               time.Minute,
+		connectionsByAgentId:   byAgentId,
+		connectionsByProjectId: byProjectId,
+		toRegister:             make(chan *ConnectedAgentInfo),
+		toUnregister:           make(chan *ConnectedAgentInfo),
+	}
+	return tr, byAgentId, byProjectId, connInfo()
 }
 
 func connInfo() *ConnectedAgentInfo {
@@ -189,48 +331,5 @@ func connInfo() *ConnectedAgentInfo {
 		ConnectionId: 123,
 		AgentId:      345,
 		ProjectId:    456,
-	}
-}
-
-func redisClient(t *testing.T) redis.UniversalClient {
-	redisURL := os.Getenv(redisURLEnvName)
-	if redisURL == "" {
-		t.Skipf("%s environment variable not set, skipping test", redisURLEnvName)
-	}
-
-	opts, err := redis.ParseURL(redisURL)
-	require.NoError(t, err)
-	return redis.NewClient(opts)
-}
-
-func getHash(t *testing.T, client redis.UniversalClient, key string) map[string]string {
-	reply, err := client.HGetAll(context.Background(), key).Result()
-	require.NoError(t, err)
-	return reply
-}
-
-func equalHash(t *testing.T, client redis.UniversalClient, key string, info *ConnectedAgentInfo) {
-	hash := getHash(t, client, key)
-	require.Len(t, hash, 1)
-	connectionIdStr := strconv.Itoa(int(info.ConnectionId))
-	require.Contains(t, hash, connectionIdStr)
-	val := hash[connectionIdStr]
-	var msg redistool.ExpiringValue
-	err := proto.Unmarshal([]byte(val), &msg)
-	require.NoError(t, err)
-	var valProto ConnectedAgentInfo
-	err = anypb.UnmarshalTo(msg.Value, &valProto, proto.UnmarshalOptions{})
-	require.NoError(t, err)
-	assert.Empty(t, cmp.Diff(info, &valProto, protocmp.Transform()))
-}
-
-func valuesExpireAfter(t *testing.T, client redis.UniversalClient, key string, expireAfter time.Time) {
-	hash := getHash(t, client, key)
-	require.NotEmpty(t, hash)
-	for _, val := range hash {
-		var msg redistool.ExpiringValue
-		err := proto.Unmarshal([]byte(val), &msg)
-		require.NoError(t, err)
-		assert.True(t, msg.ExpiresAt.AsTime().After(expireAfter))
 	}
 }
