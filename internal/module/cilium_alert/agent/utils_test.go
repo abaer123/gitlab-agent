d1090593c8e8ec04286528f74139b963b915962d
Zamir Martins Filho <zfilho@gitlab.com>
2021-02-08 00:50:00 +0000

    Add support for L4 rules for CiliumNetworkPolicies

--- internal/module/cilium_alert/agent/utils_test.go
+++ internal/module/cilium_alert/agent/utils_test.go
@@ -189,3 +189,345 @@ func TestOtherThanIngressAndEgress(t *testing.T) {
 	require.NoError(t, err)
 	assert.Nil(t, cnp)
 }
+
+func TestL4OnlyEgressMatch(t *testing.T) {
+	policy := &v2.CiliumNetworkPolicy{
+		ObjectMeta: metav1.ObjectMeta{
+			Name:        "Test",
+			Annotations: map[string]string{"app.gitlab.com/alert": "true"},
+		},
+		Spec: &api.Rule{
+			EndpointSelector: api.NewESFromLabels(labels.NewLabel("thiskey", "", "any")),
+			Egress: []api.EgressRule{
+				{
+					ToPorts: api.PortRules{
+						{
+							Ports: []api.PortProtocol{
+								{
+									Port:     "5000",
+									Protocol: api.ProtoTCP,
+								},
+							},
+						},
+					},
+				},
+			},
+		},
+	}
+	cnpList := v2.CiliumNetworkPolicyList{
+		Items: []v2.CiliumNetworkPolicy{*policy},
+	}
+
+	cnp, err := getPolicy(&flow.Flow{
+		Destination: &flow.Endpoint{
+			Namespace: "ThisNamespace",
+			Labels:    []string{"otherkey="},
+		},
+		TrafficDirection: flow.TrafficDirection_EGRESS,
+		Source:           &flow.Endpoint{Labels: []string{"thiskey="}},
+		L4: &flow.Layer4{
+			Protocol: &flow.Layer4_TCP{
+				TCP: &flow.TCP{
+					SourcePort:      8081,
+					DestinationPort: 80,
+				},
+			},
+		},
+	}, &cnpList)
+	require.NoError(t, err)
+	assert.Empty(t, cmp.Diff(cnp, policy, kube_testing.TransformToUnstructured()))
+}
+
+func TestL4OnlyUDPEgressMatch(t *testing.T) {
+	policy := &v2.CiliumNetworkPolicy{
+		ObjectMeta: metav1.ObjectMeta{
+			Name:        "Test",
+			Annotations: map[string]string{"app.gitlab.com/alert": "true"},
+		},
+		Spec: &api.Rule{
+			EndpointSelector: api.NewESFromLabels(labels.NewLabel("thiskey", "", "any")),
+			Egress: []api.EgressRule{
+				{
+					ToPorts: api.PortRules{
+						{
+							Ports: []api.PortProtocol{
+								{
+									Port:     "5000",
+									Protocol: api.ProtoUDP,
+								},
+							},
+						},
+					},
+				},
+			},
+		},
+	}
+	cnpList := v2.CiliumNetworkPolicyList{
+		Items: []v2.CiliumNetworkPolicy{*policy},
+	}
+
+	cnp, err := getPolicy(&flow.Flow{
+		Destination: &flow.Endpoint{
+			Namespace: "ThisNamespace",
+			Labels:    []string{"otherkey="},
+		},
+		TrafficDirection: flow.TrafficDirection_EGRESS,
+		Source:           &flow.Endpoint{Labels: []string{"thiskey="}},
+		L4: &flow.Layer4{
+			Protocol: &flow.Layer4_TCP{
+				TCP: &flow.TCP{
+					SourcePort:      8081,
+					DestinationPort: 5000,
+				},
+			},
+		},
+	}, &cnpList)
+	require.NoError(t, err)
+	assert.Empty(t, cmp.Diff(cnp, policy, kube_testing.TransformToUnstructured()))
+}
+
+func TestL4OnlyUDPFlowEgressMatch(t *testing.T) {
+	policy := &v2.CiliumNetworkPolicy{
+		ObjectMeta: metav1.ObjectMeta{
+			Name:        "Test",
+			Annotations: map[string]string{"app.gitlab.com/alert": "true"},
+		},
+		Spec: &api.Rule{
+			EndpointSelector: api.NewESFromLabels(labels.NewLabel("thiskey", "", "any")),
+			Egress: []api.EgressRule{
+				{
+					ToPorts: api.PortRules{
+						{
+							Ports: []api.PortProtocol{
+								{
+									Port:     "5000",
+									Protocol: api.ProtoTCP,
+								},
+							},
+						},
+					},
+				},
+			},
+		},
+	}
+	cnpList := v2.CiliumNetworkPolicyList{
+		Items: []v2.CiliumNetworkPolicy{*policy},
+	}
+
+	cnp, err := getPolicy(&flow.Flow{
+		Destination: &flow.Endpoint{
+			Namespace: "ThisNamespace",
+			Labels:    []string{"otherkey="},
+		},
+		TrafficDirection: flow.TrafficDirection_EGRESS,
+		Source:           &flow.Endpoint{Labels: []string{"thiskey="}},
+		L4: &flow.Layer4{
+			Protocol: &flow.Layer4_UDP{
+				UDP: &flow.UDP{
+					SourcePort:      8081,
+					DestinationPort: 5000,
+				},
+			},
+		},
+	}, &cnpList)
+	require.NoError(t, err)
+	assert.Empty(t, cmp.Diff(cnp, policy, kube_testing.TransformToUnstructured()))
+}
+
+func TestL4OnlyUDPFlowEgressNoMatch(t *testing.T) {
+	policy := &v2.CiliumNetworkPolicy{
+		ObjectMeta: metav1.ObjectMeta{
+			Name:        "Test",
+			Annotations: map[string]string{"app.gitlab.com/alert": "true"},
+		},
+		Spec: &api.Rule{
+			EndpointSelector: api.NewESFromLabels(labels.NewLabel("thiskey", "", "any")),
+			Egress: []api.EgressRule{
+				{
+					ToPorts: api.PortRules{
+						{
+							Ports: []api.PortProtocol{
+								{
+									Port:     "5000",
+									Protocol: api.ProtoUDP,
+								},
+							},
+						},
+					},
+				},
+			},
+		},
+	}
+	cnpList := v2.CiliumNetworkPolicyList{
+		Items: []v2.CiliumNetworkPolicy{*policy},
+	}
+
+	cnp, err := getPolicy(&flow.Flow{
+		Destination: &flow.Endpoint{
+			Namespace: "ThisNamespace",
+			Labels:    []string{"otherkey="},
+		},
+		TrafficDirection: flow.TrafficDirection_EGRESS,
+		Source:           &flow.Endpoint{Labels: []string{"thiskey="}},
+		L4: &flow.Layer4{
+			Protocol: &flow.Layer4_UDP{
+				UDP: &flow.UDP{
+					SourcePort:      8081,
+					DestinationPort: 5000,
+				},
+			},
+		},
+	}, &cnpList)
+	require.NoError(t, err)
+	assert.Nil(t, cnp)
+}
+
+func TestL4OnlyMultiplePortsEgressNoMatch(t *testing.T) {
+	policy := &v2.CiliumNetworkPolicy{
+		ObjectMeta: metav1.ObjectMeta{
+			Name:        "Test",
+			Annotations: map[string]string{"app.gitlab.com/alert": "true"},
+		},
+		Spec: &api.Rule{
+			EndpointSelector: api.NewESFromLabels(labels.NewLabel("thiskey", "", "any")),
+			Egress: []api.EgressRule{
+				{
+					ToPorts: api.PortRules{
+						{
+							Ports: []api.PortProtocol{
+								{
+									Port:     "5000",
+									Protocol: api.ProtoTCP,
+								},
+								{
+									Port:     "80",
+									Protocol: api.ProtoTCP,
+								},
+							},
+						},
+					},
+				},
+			},
+		},
+	}
+	cnpList := v2.CiliumNetworkPolicyList{
+		Items: []v2.CiliumNetworkPolicy{*policy},
+	}
+
+	cnp, err := getPolicy(&flow.Flow{
+		Destination: &flow.Endpoint{
+			Namespace: "ThisNamespace",
+			Labels:    []string{"otherkey="},
+		},
+		TrafficDirection: flow.TrafficDirection_EGRESS,
+		Source:           &flow.Endpoint{Labels: []string{"thiskey="}},
+		L4: &flow.Layer4{
+			Protocol: &flow.Layer4_TCP{
+				TCP: &flow.TCP{
+					SourcePort:      8081,
+					DestinationPort: 80,
+				},
+			},
+		},
+	}, &cnpList)
+	require.NoError(t, err)
+	assert.Nil(t, cnp)
+}
+
+func TestL4OnlyEgressNoMatch(t *testing.T) {
+	policy := &v2.CiliumNetworkPolicy{
+		ObjectMeta: metav1.ObjectMeta{
+			Name:        "Test",
+			Annotations: map[string]string{"app.gitlab.com/alert": "true"},
+		},
+		Spec: &api.Rule{
+			EndpointSelector: api.NewESFromLabels(labels.NewLabel("thiskey", "", "any")),
+			Egress: []api.EgressRule{
+				{
+					ToPorts: api.PortRules{
+						{
+							Ports: []api.PortProtocol{
+								{
+									Port:     "80",
+									Protocol: api.ProtoTCP,
+								},
+							},
+						},
+					},
+				},
+			},
+		},
+	}
+	cnpList := v2.CiliumNetworkPolicyList{
+		Items: []v2.CiliumNetworkPolicy{*policy},
+	}
+
+	cnp, err := getPolicy(&flow.Flow{
+		Destination: &flow.Endpoint{
+			Namespace: "ThisNamespace",
+			Labels:    []string{"otherkey="},
+		},
+		TrafficDirection: flow.TrafficDirection_EGRESS,
+		Source:           &flow.Endpoint{Labels: []string{"thiskey="}},
+		L4: &flow.Layer4{
+			Protocol: &flow.Layer4_TCP{
+				TCP: &flow.TCP{
+					SourcePort:      8081,
+					DestinationPort: 80,
+				},
+			},
+		},
+	}, &cnpList)
+	require.NoError(t, err)
+	assert.Nil(t, cnp)
+}
+
+func TestWithL4IngressMatch(t *testing.T) {
+	policy := &v2.CiliumNetworkPolicy{
+		ObjectMeta: metav1.ObjectMeta{
+			Name:        "Test",
+			Annotations: map[string]string{"app.gitlab.com/alert": "true"}},
+		Spec: &api.Rule{
+			EndpointSelector: api.NewESFromLabels(labels.NewLabel("thiskey", "", "any")),
+			Ingress: []api.IngressRule{
+				{
+					IngressCommonRule: api.IngressCommonRule{
+						FromEndpoints: []api.EndpointSelector{api.NewESFromLabels(labels.NewLabel("nootherkey", "", "any"))},
+					},
+					ToPorts: api.PortRules{
+						{
+							Ports: []api.PortProtocol{
+								{
+									Port:     "80",
+									Protocol: api.ProtoTCP,
+								},
+							},
+						},
+					},
+				},
+			},
+		},
+	}
+	cnpList := v2.CiliumNetworkPolicyList{
+		Items: []v2.CiliumNetworkPolicy{*policy},
+	}
+
+	cnp, err := getPolicy(&flow.Flow{
+		Source: &flow.Endpoint{
+			Namespace: "ThisNamespace",
+			Labels:    []string{"otherkey="},
+		},
+		TrafficDirection: flow.TrafficDirection_INGRESS,
+		Destination:      &flow.Endpoint{Labels: []string{"thiskey="}},
+		L4: &flow.Layer4{
+			Protocol: &flow.Layer4_TCP{
+				TCP: &flow.TCP{
+					SourcePort:      8081,
+					DestinationPort: 80,
+				},
+			},
+		},
+	}, &cnpList)
+	require.NoError(t, err)
+	assert.Empty(t, cmp.Diff(cnp, policy, kube_testing.TransformToUnstructured()))
+}
