4116e73969048032eade9c6b9f1b8aed0ab8dcd0
Mikhail Mazurskiy <mmazurskiy@gitlab.com>
2021-05-26 21:04:05 +1000

    Improve GitOps module structure

---
+++ internal/module/gitops/agent/worker_manager.go
@@ -0,0 +1,112 @@
+package agent
+
+import (
+	"context"
+	"fmt"
+
+	"gitlab.com/gitlab-org/cluster-integration/gitlab-agent/internal/tool/logz"
+	"gitlab.com/gitlab-org/cluster-integration/gitlab-agent/pkg/agentcfg"
+	"go.uber.org/zap"
+	"google.golang.org/protobuf/proto"
+	"k8s.io/apimachinery/pkg/util/wait"
+)
+
+type workerManager struct {
+	log           *zap.Logger
+	workerFactory GitopsWorkerFactory
+	workers       map[string]*gitopsWorkerHolder // project id -> worker holder instance
+}
+
+func newWorkerManager(log *zap.Logger, workerFactory GitopsWorkerFactory) *workerManager {
+	return &workerManager{
+		log:           log,
+		workerFactory: workerFactory,
+		workers:       map[string]*gitopsWorkerHolder{},
+	}
+}
+
+func (m *workerManager) startNewWorker(agentId int64, project *agentcfg.ManifestProjectCF) {
+	l := m.log.With(logz.ProjectId(project.Id))
+	l.Info("Starting synchronization worker")
+	worker := m.workerFactory.New(agentId, project)
+	ctx, cancel := context.WithCancel(context.Background())
+	workerHolder := &gitopsWorkerHolder{
+		worker:  worker,
+		project: project,
+		stop:    cancel,
+	}
+	workerHolder.wg.StartWithContext(ctx, worker.Run)
+	m.workers[project.Id] = workerHolder
+}
+
+func (m *workerManager) ApplyConfiguration(agentId int64, gitops *agentcfg.GitopsCF) error {
+	projects := gitops.ManifestProjects
+	newSetOfProjects := make(map[string]struct{}, len(projects))
+	var projectsToStartWorkersFor []*agentcfg.ManifestProjectCF
+	var workersToStop []*gitopsWorkerHolder
+
+	// Collect projects without workers or with updated configuration.
+	for _, project := range projects {
+		if _, ok := newSetOfProjects[project.Id]; ok {
+			return fmt.Errorf("duplicate project id: %s", project.Id)
+		}
+		newSetOfProjects[project.Id] = struct{}{}
+		workerHolder := m.workers[project.Id]
+		if workerHolder == nil { // New project added
+			projectsToStartWorkersFor = append(projectsToStartWorkersFor, project)
+		} else { // We have a worker for this project already
+			if proto.Equal(project, workerHolder.project) {
+				// Worker's configuration hasn't changed, nothing to do here
+				continue
+			}
+			m.log.Info("Configuration has been updated, restarting synchronization worker", logz.ProjectId(project.Id))
+			workersToStop = append(workersToStop, workerHolder)
+			projectsToStartWorkersFor = append(projectsToStartWorkersFor, project)
+		}
+	}
+
+	// Stop workers for projects which have been removed from the list.
+	for projectId, workerHolder := range m.workers {
+		if _, ok := newSetOfProjects[projectId]; ok {
+			continue
+		}
+		workersToStop = append(workersToStop, workerHolder)
+	}
+
+	// Tell workers that should be stopped to stop.
+	for _, workerHolder := range workersToStop {
+		m.log.Info("Stopping synchronization worker", logz.ProjectId(workerHolder.project.Id))
+		workerHolder.stop()
+		delete(m.workers, workerHolder.project.Id)
+	}
+
+	// Wait for stopped workers to finish.
+	for _, workerHolder := range workersToStop {
+		m.log.Info("Waiting for synchronization worker to stop", logz.ProjectId(workerHolder.project.Id))
+		workerHolder.wg.Wait()
+	}
+
+	// Start new workers for new projects or because of updated configuration.
+	for _, project := range projectsToStartWorkersFor {
+		m.startNewWorker(agentId, project)
+	}
+	return nil
+}
+
+func (m *workerManager) stopAllWorkers() {
+	// Tell all workers to stop
+	for _, workerHolder := range m.workers {
+		workerHolder.stop()
+	}
+	// Wait for all workers to stop
+	for _, workerHolder := range m.workers {
+		workerHolder.wg.Wait()
+	}
+}
+
+type gitopsWorkerHolder struct {
+	worker  GitopsWorker
+	project *agentcfg.ManifestProjectCF
+	wg      wait.Group
+	stop    context.CancelFunc
+}
---
+++ internal/module/gitops/agent/worker_manager_test.go
@@ -0,0 +1,165 @@
+package agent
+
+import (
+	"context"
+	"fmt"
+	"testing"
+
+	"github.com/golang/mock/gomock"
+	"github.com/stretchr/testify/require"
+	"gitlab.com/gitlab-org/cluster-integration/gitlab-agent/internal/tool/testing/matcher"
+	"gitlab.com/gitlab-org/cluster-integration/gitlab-agent/internal/tool/testing/testhelpers"
+	"gitlab.com/gitlab-org/cluster-integration/gitlab-agent/pkg/agentcfg"
+	"go.uber.org/zap/zaptest"
+	"google.golang.org/protobuf/proto"
+)
+
+func TestStartsWorkersAccordingToConfiguration(t *testing.T) {
+	for caseNum, config := range testConfigurations() {
+		t.Run(fmt.Sprintf("case %d", caseNum), func(t *testing.T) {
+			projects := config.GetGitops().GetManifestProjects() // nolint: scopelint
+			expectedNumberOfWorkers := len(projects)             // nolint: scopelint
+			wm, ctrl, factory := setupWM(t)
+			worker := NewMockGitopsWorker(ctrl)
+			for i := 0; i < expectedNumberOfWorkers; i++ {
+				factory.EXPECT().
+					New(testhelpers.AgentId, matcher.ProtoEq(t, projects[i])).
+					Return(worker)
+			}
+			worker.EXPECT().
+				Run(gomock.Any()).
+				Times(expectedNumberOfWorkers)
+			err := defaultAndValidateConfiguration(config) // nolint: scopelint
+			require.NoError(t, err)
+			err = wm.ApplyConfiguration(testhelpers.AgentId, config.Gitops) // nolint: scopelint
+			require.NoError(t, err)
+		})
+	}
+}
+
+func TestUpdatesWorkersAccordingToConfiguration(t *testing.T) {
+	normalOrder := testConfigurations()
+	reverseOrder := testConfigurations()
+	reverse(reverseOrder)
+	tests := []struct {
+		name    string
+		configs []*agentcfg.AgentConfiguration
+	}{
+		{
+			name:    "normal order",
+			configs: normalOrder,
+		},
+		{
+			name:    "reverse order",
+			configs: reverseOrder,
+		},
+	}
+	for _, tc := range tests {
+		t.Run(tc.name, func(t *testing.T) {
+			numProjects := numUniqueProjects(tc.configs) // nolint: scopelint
+			wm, ctrl, factory := setupWM(t)
+			worker := NewMockGitopsWorker(ctrl)
+			worker.EXPECT().
+				Run(gomock.Any()).
+				Do(func(ctx context.Context) {
+					<-ctx.Done()
+				}).
+				Times(numProjects)
+			factory.EXPECT().
+				New(testhelpers.AgentId, gomock.Any()).
+				Return(worker).
+				Times(numProjects)
+			for _, config := range tc.configs { // nolint: scopelint
+				err := defaultAndValidateConfiguration(config)
+				require.NoError(t, err)
+				err = wm.ApplyConfiguration(testhelpers.AgentId, config.Gitops)
+				require.NoError(t, err)
+			}
+		})
+	}
+}
+
+func setupWM(t *testing.T) (*workerManager, *gomock.Controller, *MockGitopsWorkerFactory) {
+	ctrl := gomock.NewController(t)
+	workerFactory := NewMockGitopsWorkerFactory(ctrl)
+	wm := newWorkerManager(zaptest.NewLogger(t), workerFactory)
+	t.Cleanup(wm.stopAllWorkers)
+	return wm, ctrl, workerFactory
+}
+
+func numUniqueProjects(cfgs []*agentcfg.AgentConfiguration) int {
+	num := 0
+	projects := make(map[string]*agentcfg.ManifestProjectCF)
+	for _, config := range cfgs {
+		for _, proj := range config.GetGitops().GetManifestProjects() {
+			old, ok := projects[proj.Id]
+			if ok {
+				if !proto.Equal(old, proj) {
+					projects[proj.Id] = proj
+					num++
+				}
+			} else {
+				projects[proj.Id] = proj
+				num++
+			}
+		}
+	}
+	return num
+}
+
+func testConfigurations() []*agentcfg.AgentConfiguration {
+	const (
+		project1 = "bla1/project1"
+		project2 = "bla1/project2"
+		project3 = "bla3/project3"
+	)
+	return []*agentcfg.AgentConfiguration{
+		{
+			AgentId: testhelpers.AgentId,
+		},
+		{
+			Gitops: &agentcfg.GitopsCF{
+				ManifestProjects: []*agentcfg.ManifestProjectCF{
+					{
+						Id: project1,
+					},
+				},
+			},
+			AgentId: testhelpers.AgentId,
+		},
+		{
+			Gitops: &agentcfg.GitopsCF{
+				ManifestProjects: []*agentcfg.ManifestProjectCF{
+					{
+						Id:               project1,
+						DefaultNamespace: "abc", // update config
+					},
+					{
+						Id: project2,
+					},
+				},
+			},
+			AgentId: testhelpers.AgentId,
+		},
+		{
+			Gitops: &agentcfg.GitopsCF{
+				ManifestProjects: []*agentcfg.ManifestProjectCF{
+					{
+						Id: project3,
+					},
+					{
+						Id:               project2,
+						DefaultNamespace: "abc", // update config
+					},
+				},
+			},
+			AgentId: testhelpers.AgentId,
+		},
+	}
+}
+
+func reverse(cfgs []*agentcfg.AgentConfiguration) {
+	for i, j := 0, len(cfgs)-1; i < j; i, j = i+1, j-1 {
+		cfgs[i], cfgs[j] = cfgs[j], cfgs[i]
+	}
+}
