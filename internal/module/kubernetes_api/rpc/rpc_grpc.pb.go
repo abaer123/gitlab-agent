d30d0dff23f3105f9b83c065c2a407bfe3b545c5
Mikhail Mazurskiy <mmazurskiy@gitlab.com>
2021-05-27 12:07:15 +1000

    Generated

--- internal/module/kubernetes_api/rpc/rpc_grpc.pb.go
+++ internal/module/kubernetes_api/rpc/rpc_grpc.pb.go
@@ -5,7 +5,7 @@ package rpc
 import (
 	context "context"
 
-	grpctool "gitlab.com/gitlab-org/cluster-integration/gitlab-agent/internal/tool/grpctool"
+	grpctool "gitlab.com/gitlab-org/cluster-integration/gitlab-agent/v14/internal/tool/grpctool"
 	grpc "google.golang.org/grpc"
 	codes "google.golang.org/grpc/codes"
 	status "google.golang.org/grpc/status"
