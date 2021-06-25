aedd82a5f04bb7338ccd6373bb442edf938d617b
Mikhail Mazurskiy <mmazurskiy@gitlab.com>
2021-06-25 17:00:27 +1000

    Retry synchronization on error and periodically

--- internal/module/gitops/agent/sync_worker.go
+++ internal/module/gitops/agent/sync_worker.go
@@ -4,8 +4,11 @@ import (
 	"bytes"
 	"context"
 	"os"
+	"time"
 
 	"gitlab.com/gitlab-org/cluster-integration/gitlab-agent/v14/internal/tool/errz"
+	"gitlab.com/gitlab-org/cluster-integration/gitlab-agent/v14/internal/tool/logz"
+	"gitlab.com/gitlab-org/cluster-integration/gitlab-agent/v14/internal/tool/retry"
 	"go.uber.org/zap"
 	"k8s.io/apimachinery/pkg/apis/meta/v1/unstructured"
 	"k8s.io/cli-runtime/pkg/genericclioptions"
@@ -23,29 +26,30 @@ type syncJob struct {
 }
 
 type syncWorker struct {
-	log          *zap.Logger
-	applier      Applier
-	applyOptions apply.Options
+	log             *zap.Logger
+	reapplyInterval time.Duration
+	applier         Applier
+	applierBackoff  retry.BackoffManager
+	applyOptions    apply.Options
 }
 
-func newSyncWorker(log *zap.Logger, applier Applier, applyOptions apply.Options) *syncWorker {
-	return &syncWorker{
-		log:          log,
-		applier:      applier,
-		applyOptions: applyOptions,
-	}
-}
-
-func (s *syncWorker) run(jobs <-chan syncJob) {
+func (s *syncWorker) Run(jobs <-chan syncJob) {
 	for job := range jobs {
-		err := s.synchronize(job)
-		if err != nil {
-			if errz.ContextDone(err) {
-				s.log.Info("Synchronization was canceled", zap.Error(err))
-			} else {
-				s.log.Warn("Synchronization failed", zap.Error(err))
+		l := s.log.With(logz.CommitId(job.commitId))
+		_ = retry.PollWithBackoff(job.ctx, s.applierBackoff, true, s.reapplyInterval, func() (error, retry.AttemptResult) {
+			l.Info("Synchronizing objects")
+			err := s.synchronize(job)
+			if err != nil {
+				if errz.ContextDone(err) {
+					l.Info("Synchronization was canceled", zap.Error(err))
+				} else {
+					l.Warn("Synchronization failed", zap.Error(err))
+				}
+				return nil, retry.Backoff
 			}
-		}
+			l.Info("Objects synchronized")
+			return nil, retry.Continue
+		})
 	}
 }
 
