bdcc5a2db44cc21bd4265b2d6712ee05ca5a579a
Mikhail Mazurskiy <mmazurskiy@gitlab.com>
2021-07-27 19:21:12 +1000

    Refactor GitOps poller

--- internal/module/agent_configuration/server/server.go
+++ internal/module/agent_configuration/server/server.go
@@ -1,6 +1,7 @@
 package server
 
 import (
+	"context"
 	"time"
 
 	"gitlab.com/gitlab-org/cluster-integration/gitlab-agent/v14/internal/api"
@@ -9,8 +10,11 @@ import (
 	"gitlab.com/gitlab-org/cluster-integration/gitlab-agent/v14/internal/module/agent_tracker"
 	"gitlab.com/gitlab-org/cluster-integration/gitlab-agent/v14/internal/module/modserver"
 	"gitlab.com/gitlab-org/cluster-integration/gitlab-agent/v14/internal/tool/grpctool"
+	"gitlab.com/gitlab-org/cluster-integration/gitlab-agent/v14/internal/tool/logz"
 	"gitlab.com/gitlab-org/cluster-integration/gitlab-agent/v14/internal/tool/mathz"
 	"gitlab.com/gitlab-org/cluster-integration/gitlab-agent/v14/internal/tool/retry"
+	"gitlab.com/gitlab-org/cluster-integration/gitlab-agent/v14/pkg/agentcfg"
+	"go.uber.org/zap"
 	"google.golang.org/protobuf/types/known/timestamppb"
 )
 
@@ -25,23 +29,62 @@ type server struct {
 }
 
 func (s *server) GetConfiguration(req *rpc.ConfigurationRequest, server rpc.AgentConfiguration_GetConfigurationServer) error {
+	connectedAgentInfo := &agent_tracker.ConnectedAgentInfo{
+		AgentMeta:    req.AgentMeta,
+		ConnectedAt:  timestamppb.Now(),
+		ConnectionId: mathz.Int63(),
+	}
+	defer s.maybeUnregisterAgent(connectedAgentInfo)
 	ctx := server.Context()
+	log := grpctool.LoggerFromContext(ctx)
+	agentToken := api.AgentTokenFromContext(ctx)
 	p := pollJob{
 		ctx:                      ctx,
-		log:                      grpctool.LoggerFromContext(ctx),
 		api:                      s.api,
 		gitaly:                   s.gitaly,
-		agentRegisterer:          s.agentRegisterer,
-		server:                   server,
-		agentToken:               api.AgentTokenFromContext(ctx),
+		newConfigCb:              s.onNewConfig(server),
 		maxConfigurationFileSize: s.maxConfigurationFileSize,
 		lastProcessedCommitId:    req.CommitId,
-		connectedAgentInfo: &agent_tracker.ConnectedAgentInfo{
-			AgentMeta:    req.AgentMeta,
-			ConnectedAt:  timestamppb.Now(),
-			ConnectionId: mathz.Int63(),
-		},
 	}
-	defer p.Cleanup()
-	return s.api.PollWithBackoff(server, s.getConfigurationBackoff(), true, s.getConfigurationPollPeriod, p.Attempt)
+	return s.api.PollWithBackoff(server, s.getConfigurationBackoff(), true, s.getConfigurationPollPeriod, func() (error, retry.AttemptResult) {
+		// This call is made on each poll because:
+		// - it checks that the agent's token is still valid
+		// - repository location in Gitaly might have changed
+		agentInfo, err := s.api.GetAgentInfo(ctx, log, agentToken)
+		if err != nil {
+			return err, retry.Done
+		}
+		s.maybeRegisterAgent(ctx, connectedAgentInfo, agentInfo)
+		logWithFields := log.With(logz.AgentId(agentInfo.Id), logz.ProjectId(agentInfo.Repository.GlProjectPath))
+		return p.Attempt(logWithFields, agentInfo)
+	})
+}
+
+func (s *server) onNewConfig(server rpc.AgentConfiguration_GetConfigurationServer) newConfigCb {
+	return func(log *zap.Logger, config *agentcfg.AgentConfiguration, commitId string) (error, retry.AttemptResult) {
+		err := server.Send(&rpc.ConfigurationResponse{
+			Configuration: config,
+			CommitId:      commitId,
+		})
+		if err != nil {
+			return s.api.HandleSendError(log, "Config: failed to send config", err), retry.Done
+		}
+		return nil, retry.Continue
+	}
+}
+
+func (s *server) maybeRegisterAgent(ctx context.Context, connectedAgentInfo *agent_tracker.ConnectedAgentInfo, agentInfo *api.AgentInfo) {
+	if connectedAgentInfo.AgentId != 0 {
+		return
+	}
+	connectedAgentInfo.AgentId = agentInfo.Id
+	connectedAgentInfo.ProjectId = agentInfo.ProjectId
+	s.agentRegisterer.RegisterConnection(ctx, connectedAgentInfo)
+}
+
+func (s *server) maybeUnregisterAgent(connectedAgentInfo *agent_tracker.ConnectedAgentInfo) {
+	if connectedAgentInfo.AgentId == 0 {
+		return
+	}
+	s.agentRegisterer.UnregisterConnection(context.Background(), connectedAgentInfo)
 }
